4c2e46cbea7ae2bda1bbf15038594922e10dbc98
emi-8086 <or715.22.a.s@gmail.com>
2026-05-05 22:31:46 -0600

    arreglos, por favor que funcione

--- app-autenticacion/vite.config.js
+++ app-autenticacion/vite.config.js
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Toda petición a /api se redirige al servidor real
-      '/api': {
-        target: 'https://sii.celaya.tecnm.mx',
-        changeOrigin: true,   // cambia el header Host al del target
-        secure: true,         // verifica certificado SSL
-        // Si el servidor tiene problemas de SSL, cambia a: secure: false
-      },
-    },
-  },
+  base: './',
 })
  
