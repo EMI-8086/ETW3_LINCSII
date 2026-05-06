9d3ce9543c56cfd26579e3297ccb98396fa2be0d
emi-8086 <or715.22.a.s@gmail.com>
2026-05-06 07:09:56 -0600

    más arreglos para el deploy

---
+++ app-autenticacion/you/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  server: {
+    proxy: {
+      '/api/proxy': {
+        target: 'https://sii.celaya.tecnm.mx',
+        changeOrigin: true,
+        rewrite: () => '/api/login',
+        secure: false,
+      },
+      '/api/movil': {
+        target: 'https://sii.celaya.tecnm.mx',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
