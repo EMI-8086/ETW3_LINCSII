acf22209d05f67be92e2fe18ee5bda98ad3beb58
emi-8086 <or715.22.a.s@gmail.com>
2026-05-06 06:58:55 -0600

    arreglo forma local

--- app-autenticacion/vite.config.js
+++ app-autenticacion/vite.config.js
@@ -4,5 +4,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    proxy: {
+      '/api/proxy': {
+        target: 'https://sii.celaya.tecnm.mx',
+        changeOrigin: true,
+        rewrite: () => '/api/login',
+        secure: false,
+      },
+      '/api/movil': {
+        target: 'https://sii.celaya.tecnm.mx',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
- 
