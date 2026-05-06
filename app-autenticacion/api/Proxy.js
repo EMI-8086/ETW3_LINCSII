b1dfa67b3ec141e23b1a3da428efd66397a46665
emi-8086 <or715.22.a.s@gmail.com>
2026-05-06 06:52:16 -0600

    otros cambios

--- app-autenticacion/api/Proxy.js
+++ app-autenticacion/api/Proxy.js
@@ -1,3 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  server: {
+    proxy: {
+      '/api/proxy': {
+        target: 'https://sii.celaya.tecnm.mx',
+        changeOrigin: true,
+        rewrite: (path) => '/api/login',
+      },
+      '/api/movil': {
+        target: 'https://sii.celaya.tecnm.mx',
+        changeOrigin: true,
+      }
+    }
+  }
+})
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
